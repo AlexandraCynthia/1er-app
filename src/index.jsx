77a690c0ab809068027ab6ca1e385f819e6d6aee
amendoza <alexandracynthia1663@gmail.com>
2023-02-01 23:21:20 -0500

    1era pre entrega

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Varela+Round&display=swap" rel="stylesheet"></link>
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
